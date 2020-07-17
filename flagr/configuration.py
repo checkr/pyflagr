d2322e298db2040e3306a8d9fe2bcdecc68eb862
zhuojie <zhuojie@checkr.com>
2020-07-17 14:15:39 -0700

    Bump version

--- flagr/configuration.py
+++ flagr/configuration.py
@@ -241,5 +241,5 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 1.1.10\n"\
-               "SDK Package Version: 1.1.0".\
+               "SDK Package Version: 1.1.10".\
                format(env=sys.platform, pyversion=sys.version)
